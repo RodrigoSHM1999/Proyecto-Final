00819c072b7c8496be9bb16bcc52f4474d90da92
RodrigoSHM1999 <rhumanma@unsa.edu.pe>
2020-08-10 12:40:22 -0500

    agregando cliente y compra

--- Supermercado/market/models.py
+++ Supermercado/market/models.py
@@ -9,3 +9,17 @@ class Product(models.Model):
 	price = models.IntegerField()
 	desc = models.IntegerField()
 	offer = models.BooleanField(default=False)
+	
+class Client(models.Model):
+	id = models.AutoField(primary_key = True)
+	name = models.CharField(max_length=100)
+	dni = models.IntegerField
+	date = models.DateField('fecha regitro', blank= False,null= False)
+	status = models.BooleanField(default=False)
+
+#on_delete: al eliminar elimina la relacion 
+class Compra(models.Model):
+	id = models.AutoField(primary_key = True)
+	client_id = models.OneToOneField(Client, on_delete = models.CASCADE)
+	monto = models.IntegerField
+	date = models.DateField('fecha de compra ', blank = False, null = False)	
